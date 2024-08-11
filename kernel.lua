84679233e6ef3d67b018be3f7811fad042efbda6
Corruda <107373242+Corruda@users.noreply.github.com>
2024-08-11 13:40:02 -0400

    Add files via upload

---
+++ kernel.lua
@@ -0,0 +1,25 @@
+os.pullEventRaw()
+
+local permission = 0
+
+
+local errorCode = ""
+
+term.clear()
+term.setCursorPos(1,1)
+print("Crossline OS Kernel")
+
+local function RedScreenOfDeath()
+term.clear()
+term.setBackgroundColor(colors.red)
+
+term.setCursorPos(1,1)
+term.write("Kernel panic: " .. errorCode)
+
+end
+
+local id = multishell.launch({}, ".sys/debug.lua")
+multishell.setTitle(id, "")
+
+return {errorCode, RedScreenOfDeath }
+
