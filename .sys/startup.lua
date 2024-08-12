8bd4e567e12038e3d5dbd02b43112d0f9efbf846
Corruda <107373242+Corruda@users.noreply.github.com>
2024-08-12 15:01:01 -0400

    Update startup.lua

--- .sys/startup.lua
+++ .sys/startup.lua
@@ -1,34 +1,54 @@
 os.pullEventRaw()
-
+ 
 term.setBackgroundColor(colors.blue)
 term.clear()
-
+ 
 term.setCursorPos(1,1)
 print("Crossline OS install will now begin shortly...")
 print("----------------------------------------------")
-
+ 
 textutils.slowPrint("Deleting existing files")
 shell.run("delete",".sys")
 print("---------------------------------------------")
 sleep(1)
-
+ 
 textutils.slowPrint("Enjoy the wonders of built in Security")
 sleep(1)
-
+ 
 print("---------------------------------------------------")
-
-
+ 
+ 
 -- Install the basalt UI Library
 textutils.slowPrint("Installing UI Library")
 sleep(1)
 shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua")
-
+ 
 textutils.slowPrint("Installing Crossline OS ...")
-
+ 
 if fs.exists("disk/") then
     shell.run("delete","startup.lua")
     shell.run("cp","disk/.sys", ".")
-
+    
+    multishell.setTitle(multishell.getCurrent(), "Setup")
+    
+    textutils.slowPrint("Do you have a monitor connected to the computer? Y/N")
+    
+    answer = read()
+    
+    if answer == "Y" then
+    settings.define("connected.monitor", {
+        description = "Whether monitor is connected or not",
+        default = 1,
+        type = "number"
+        })
+    end
+    
+    settings.define("connected.monitor", {
+        description = "Whether monitor is connected or not",
+        default = 0,
+        type = "number"
+    })
+ 
     disk.eject("top")
     disk.eject("bottom")
     disk.eject("left")
@@ -36,19 +56,20 @@ if fs.exists("disk/") then
     disk.eject("back")
     disk.eject("left")
     disk.eject("right")
-
+ 
     textutils.slowPrint("Rebooting...")
     sleep(1)
     os.reboot()
-
+ 
 else
     term.clear()
     term.setBackgroundColor(colors.red)
     term.clear()
-
+ 
     term.setCursorPos(1,5)
     textutils.slowPrint("It would appear there is no disk directory.\n\nFAQ: How can this be fixed?\n\n1. If the floppy disk is not inserted insert it back into the disk drive\n\nRebooting now...")
     sleep(3)
     os.reboot()
 end
-
+ 
+ 
