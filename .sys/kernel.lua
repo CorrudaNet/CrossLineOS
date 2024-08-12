dda50e2928c5264f9d379dc479577fe62afbc588
Corruda <107373242+Corruda@users.noreply.github.com>
2024-08-12 15:02:01 -0400

    Update kernel.lua

--- .sys/kernel.lua
+++ .sys/kernel.lua
@@ -1,28 +1,32 @@
-os.pullEventRaw()
-
 local permission = 0
-
-
+ 
+ 
 local errorCode = ""
-
+ 
+local monitor = peripheral.wrap("top")
+monitor.write("Hello, From the kernel")
+ 
 term.clear()
 term.setCursorPos(1,1)
 print("Crossline OS Kernel")
-
+ 
+shell.execute(".sys/menu.lua")
+ 
 local function RedScreenOfDeath()
 term.clear()
 term.setBackgroundColor(colors.red)
-
+ 
 term.setCursorPos(1,1)
 term.write("Kernel panic: ", errorCode)
-
+ 
 sleep(3)
 os.reboot()
-
+ 
 end
-
-local id = multishell.launch({}, ".sys/debug.lua")
-multishell.setTitle(id, "")
-
+ 
+local id = multishell.launch({}, ".sys/update.lua") 
+multishell.setTitle(id, "Manual Update")
+ 
 return {errorCode, RedScreenOfDeath }
-
+ 
+ 
